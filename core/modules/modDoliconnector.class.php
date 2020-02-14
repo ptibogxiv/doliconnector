51e4a0969d4132ef4547528480c85662ebcd89bc
ptibogxiv <support@ptibogxiv.net>
2020-02-14 17:09:51 +0100

    Update modDoliconnector.class.php

--- core/modules/modDoliconnector.class.php
+++ core/modules/modDoliconnector.class.php
@@ -55,7 +55,7 @@ class modDoliconnector extends DolibarrModules {
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Module doliconnector";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '11.0.0';
+		$this->version = '11.0.1';
 		// Module description used if translation string 'ModuleXXXDesc' not found (XXX is id value)
     $this->editor_name = 'ptibogxiv.net';
     $this->editor_url = 'https://www.ptibogxiv.net';
