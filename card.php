e587b4aedab6a439231267751a075d782d2fff5a
ptibogxiv <support@ptibogxiv.net>
2022-04-12 11:15:25 +0200

    Update card.php

--- card.php
+++ card.php
@@ -289,7 +289,7 @@ print $langs->trans("NoSync");
 if ($result) {     
 foreach ($result as $user ) { 
 $wordpress->getThirdparty($user->id, '1');
-if ($wordpress->fk_soc > 0) $wdpr = $societeaccount->getCustomerAccount($wordpress->fk_soc, 'wordpress', '1');
+if (isset($wordpress->fk_soc) && $wordpress->fk_soc > 0) $wdpr = $societeaccount->getCustomerAccount($wordpress->fk_soc, 'wordpress', '1');
 print "<tr ".$bc[$var]."><td>";
 print $user->name;
 print '</td><td>'.$user->slug.'</td>';
