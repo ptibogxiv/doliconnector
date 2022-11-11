a1a0bfab118c934a57721b672b66b69109dcdfd2
ptibogxiv <support@ptibogxiv.net>
2022-11-11 19:13:44 +0100

    16.0.2

--- core/modules/modDoliconnector.class.php
+++ core/modules/modDoliconnector.class.php
@@ -55,10 +55,10 @@ class modDoliconnector extends DolibarrModules {
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Module doliconnector";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '16.0.1';
+		$this->version = '16.0.2';
 		// Module description used if translation string 'ModuleXXXDesc' not found (XXX is id value)
-		$this->editor_name = 'ptibogxiv.net';
-		$this->editor_url = 'https://www.ptibogxiv.net';
+		$this->editor_name = 'ptibogxiv.eu';
+		$this->editor_url = 'https://www.ptibogxiv.eu';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_' . strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
